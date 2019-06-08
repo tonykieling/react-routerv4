c691b75c7773ec22b13a165b2860c0f078a9970e
Tony Kieling <tony.kieling@gmail.com>
2019-06-08 11:06:03 -0700

    form added to Admin.js

--- src/Admin.js
+++ src/Admin.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react'
 
 export default class Home extends Component {
 
+  state = {
+    name: ""
+  }
+
+  handleChange = e => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    console.log("e: ", JSON.stringify(e))
+  }
+
   render() {
-    console.log("this.props: ", this.props)
     return (
       <div>
         <h1>Admin Page</h1>
         <p>This is an Admin area</p>
+      
+        <form onSubmit={this.handleSubmit} >
+          <label>
+            Name:
+            <input type= "text" name= "name" value= {this.state.name} onChange={this.handleChange} placeholder= "name, please"
+            />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+
       </div>
     )
   }
